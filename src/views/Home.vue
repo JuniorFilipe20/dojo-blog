5940d174fed79e8894c489d560a0809590e68135
Júnior Filipe <81979495+JuniorFilipe20@users.noreply.github.com>
2021-06-25 17:25:37 -0100

    begining

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
     home
+    <p> My name is {{ name }} and my age is {{ age }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    let name = 'junior'
+    let age = 25
+
+    return { name, age }
+  }
 }
 </script>
